0c56aa1ac963d74ae5378e5aca2204db9957deb8
ChaimaAitAli <151151457+ChaimaAitAli@users.noreply.github.com>
2023-12-10 23:35:12 +0100

    Javascript changes

--- BurgerWebsite/BurgerBuilder.js
+++ BurgerWebsite/BurgerBuilder.js
@@ -9,19 +9,15 @@ document.addEventListener('DOMContentLoaded', function () {
 
         mainButton.addEventListener('click', function () {
             if (!parentActivated) {
-                // Vérifier si un bouton parent a déjà été activé
                 if (lastActivatedParent !== null) {
-                    // Si oui, ne rien faire
+                    
                     return;
                 }
 
-                // Activer le bouton parent et afficher la liste de boutons fils
                 subButtonList.style.display = 'block';
-                //mainButton.style.backgroundColor = '#b7b7b7';
                 parentActivated = true;
                 lastActivatedParent = index;
 
-                // Désactiver les autres boutons parents et cacher leurs listes de boutons fils
                 parentButtons.forEach(function (otherButton, otherIndex) {
                     if (index !== otherIndex) {
                         const otherMainButton = otherButton.querySelector('button');
@@ -31,23 +27,36 @@ document.addEventListener('DOMContentLoaded', function () {
                     }
                 });
             } else {
-                // Cacher la liste de boutons fils du bouton parent actuel
                 subButtonList.style.display = 'none';
-
-                // Changer la couleur du bouton parent actuel
-              //  mainButton.style.backgroundColor = '#3498db';
             }
         });
 
-        // Ajouter des événements pour les boutons fils (si nécessaire)
         const subButtons = subButtonList.querySelectorAll('button');
 
         subButtons.forEach(function (subButton) {
             subButton.addEventListener('click', function () {
-                // Changer la couleur du bouton parent actuel
-             //   mainButton.style.backgroundColor = '#3498db';
+                
+                subButton.style.backgroundColor = '#FF7233';
+                subButton.style.Color = 'white';
 
-                // Passer automatiquement au bouton parent suivant (s'il existe)
+                
+                const previousSubButton = subButtonList.querySelector('.selected');
+                if (previousSubButton && previousSubButton !== subButton) {
+                    previousSubButton.style.backgroundColor = '#FFD6BD';
+                }
+
+                
+                subButton.classList.add('selected');
+
+                
+                for (let i = 0; i < lastActivatedParent; i++) {
+                    const previousParentButton = parentButtons[i].querySelector('button');
+                    const previousSubButtonList = parentButtons[i].querySelector('.subButtonList');
+                    previousSubButtonList.style.pointerEvents = 'none';
+                    previousParentButton.style.backgroundColor = '#b7b7b7';
+                }
+
+                
                 if (index < parentButtons.length - 1) {
                     const nextMainButton = parentButtons[index + 1].querySelector('button');
                     const nextSubButtonList = parentButtons[index + 1].querySelector('.subButtonList');
